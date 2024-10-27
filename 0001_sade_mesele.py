b01bd8a5f8bcf515dfc7968d7b9f352d26785c89
Mehri Quluzadə <mehricyber@gmail.com>
2024-10-27 13:44:37 -0400

    0001_sade_mesele.py

---
+++ 0001_sade_mesele.py
@@ -0,0 +1,5 @@
+#programda bizden 10 ve 99 arasi eded daxil edilen zaman onun  mertebelerin(onluq ve teklik) ayri-ayriliqda bosluqla yazmaqimizi isteyir
+a=int(input())#10-99 arasi eded daxil edirik
+b=a//10#a'in 10'a natamam qismeti bize birinci ededi verir biz onu b adli deyisene menimsedirik
+i=a%10# a'in 10'a qaligi ise bize teklik mertebesin veriri bunu da i adli ikinci deyisene menimsedirik
+print(b,i)#burada ise ardicil olmagi ucun onluq mertebedeki ededi birinci ikinci ise teklik mertebesindeki ededi cixisa veririk 
