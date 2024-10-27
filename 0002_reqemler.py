95328478f5b1605c938ec438ad88246dde9aa986
Mehri Quluzadə <mehricyber@gmail.com>
2024-10-27 13:53:54 -0400

    0002_reqemler.py

---
+++ 0002_reqemler.py
@@ -0,0 +1,5 @@
+#bu sualda bize  bildirilir ki, musbet n ededi daxil edirik (0<=n<=2*10^9) ve bu n ededindeki reqemlerin sayin tapmagimizi isteyir
+n=input()#n'i setir kimi daxil edirik
+n=list(n)#n'i setir kimi daxil etdiyimiz ucun daxilindeki her bir elementi siyahilasdirmaq ucun list'den istifade edirik
+s=len(n)#burada s adli deyisen yaradiriq bu deyisen reqemlerin sayin temmsil edir daha sonra n siyahsiini len ile elementlerin(reqemlerin) sayin s deyisenine menimsedirik
+print(s)#print s deyiseni ile ise reqemlerin sayin cixisa veririk
